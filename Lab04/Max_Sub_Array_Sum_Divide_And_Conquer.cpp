90a39448d2d876e51b92b86d61435fb3202cfd64
Niraj Aghera <112802707+Niraj-Node@users.noreply.github.com>
2024-01-06 22:31:42 +0530

    Refactor: Clarified subset size definitions in comments

--- Lab04/Max_Sub_Array_Sum_Divide_And_Conquer.cpp
+++ Lab04/Max_Sub_Array_Sum_Divide_And_Conquer.cpp
@@ -1,3 +1,18 @@
+/*
+
+Test 0: 
+9
+-2 1 -3 4 -1 2 1 -5 4
+Answer = 6
+
+Test 1:
+4
+-2 -5 -1 -3
+Answer = 0 (Minimum Subset Size is 0)
+Answer = -1 (Minimum Subset Size is 1)
+
+*/
+
 //{ Driver Code Starts
 #include <stdio.h>
 #include <bits/stdc++.h>
@@ -8,8 +23,12 @@ class Solution
     private:
     int maxCrossingSum(int arr[], int l, int mid, int h) 
     {
-        int sum = 0; 
-        int maxL = 0; 
+        int sum = 0;
+        int maxL = 0; // Minimum Subset Size is 0 (Empty)
+        /*  
+            maxL = INT_MIN for
+            Minimum Subset Size ~ 1
+        */
         for(int i = mid; i >= l; i--) 
         { 
             sum = sum + arr[i]; 
@@ -18,11 +37,15 @@ class Solution
         } 
     
         sum = 0; 
-        int maxR = 0; 
-        for (int i = mid+1; i <= h; i++) 
+        int maxR = 0; // Minimum Subset Size is 0 (Empty)
+        /*  
+            maxR = INT_MIN for
+            Minimum Subset Size ~ 1
+        */
+        for(int i = mid+1; i <= h; i++) 
         { 
             sum = sum + arr[i]; 
-            if (sum > maxR) 
+            if(sum > maxR) 
                 maxR = sum; 
         } 
 
@@ -38,9 +61,9 @@ class Solution
         int mid = (h-l)/2 + l; 
         int maxL = maxSubArraySum(arr, l, mid);
         int maxR = maxSubArraySum(arr, mid + 1, h);
-        int maxi = maxCrossingSum(arr, l, mid, h);
+        int CrossSum = maxCrossingSum(arr, l, mid, h);
         
-        return max(max(maxL,maxR),maxi);
+        return max(max(maxL,maxR),CrossSum);
     } 
 };
 
